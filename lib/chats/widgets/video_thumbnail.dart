5a98bcfd4262d56d50f234d8d7753328a6572e5e
Bartek Kaczmarek <dodgers196@gmail.com>
2022-01-02 12:09:26 +0100

    WIP

--- lib/chats/widgets/video_thumbnail.dart
+++ lib/chats/widgets/video_thumbnail.dart
@@ -22,7 +22,10 @@ class _VideoThumbnailState extends State<VideoThumbnail> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.file(widget.video)..initialize();
+    _controller = VideoPlayerController.file(widget.video)
+      ..initialize().then((_) {
+        setState(() {});
+      });
   }
 
   @override
@@ -33,23 +36,27 @@ class _VideoThumbnailState extends State<VideoThumbnail> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      VideoPlayer(_controller),
-      Align(
-        alignment: Alignment.bottomRight,
-        child: Container(
-          margin: const EdgeInsets.all(5.0),
-          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 2.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: Colors.black54,
-          ),
-          child: Text(
-            '${_controller.value.duration.inSeconds}s',
-            style: const TextStyle(color: Colors.white),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Stack(children: [
+        VideoPlayer(_controller),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Container(
+            margin: const EdgeInsets.all(5.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 2.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.black54,
+            ),
+            child: Text(
+              '${_controller.value.duration.inSeconds}s',
+              style: const TextStyle(color: Colors.white),
+            ),
           ),
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 }
